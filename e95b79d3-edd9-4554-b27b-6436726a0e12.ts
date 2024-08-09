c06dfb4b84684cfb44fc1d74ffd9e3601f3169a9
Your Name <you@example.com>
2024-08-10 00:23:39 +0800

    1

---
+++ e95b79d3-edd9-4554-b27b-6436726a0e12.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "e95b79d3-edd9-4554-b27b-6436726a0e12",
+    "name": "毛泽东传（三）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "逄先知",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives8/main/e95b79d3-edd9-4554-b27b-6436726a0e12.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "毛泽东传（三）",
+        "authors": [
+          "逄先知"
+        ],
+        "page_start": 1,
+        "page_end": 472,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "e95b79d3-edd9-4554-b27b-6436726a0e12.pdf",
+  "resource_type": "book",
+  "version": 2
+}
