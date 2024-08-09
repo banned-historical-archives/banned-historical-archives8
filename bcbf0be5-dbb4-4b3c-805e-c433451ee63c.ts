e631a6bb846b56d212660f352c343ebfb753073d
Your Name <you@example.com>
2024-08-10 00:24:30 +0800

    1

---
+++ bcbf0be5-dbb4-4b3c-805e-c433451ee63c.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "bcbf0be5-dbb4-4b3c-805e-c433451ee63c",
+    "name": "毛泽东传（二）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "逄先知",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives8/main/bcbf0be5-dbb4-4b3c-805e-c433451ee63c.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "毛泽东传（二）",
+        "authors": [
+          "逄先知"
+        ],
+        "page_start": 1,
+        "page_end": 510,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "bcbf0be5-dbb4-4b3c-805e-c433451ee63c.pdf",
+  "resource_type": "book",
+  "version": 2
+}
