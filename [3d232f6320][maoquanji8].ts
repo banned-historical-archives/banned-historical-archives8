6592118833031a3382292dd5bf1483a2195c7167
Your Name <you@example.com>
2025-03-04 04:37:50 +0000

    feat: [OCR Patch]致彭德怀等电[毛泽东全集第8卷

---
+++ [3d232f6320][maoquanji8].ts
@@ -0,0 +1,4 @@
+
+export default [
+  {"version":2,"parts":{"3":{"diff":"=18\t-32\t+%0A%0A"}},"comments":{},"description":"","newComments":["[1]这是毛泽东和周恩来致彭德怀、杨尚昆、林彪、聂荣臻、朱瑞、徐海东、程子华、郭述申电的主要内容。"]},
+];
